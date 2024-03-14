1fca9920c213cbfba48734524f5cb1300c5ca82c
Brandon Fyall <BrandonFyall@Brandons-MacBook-Pro-2.local>
2024-03-14 16:47:30 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg'
-// import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
@@ -9,11 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-// const app = new Clarifai.App({
-//   apiKey: '4c7d0bd276e24f919bd6f584b83e90f0'
-// });
-
 
 const returnClarifai = (imageUrl) => {
   // Your PAT (Personal Access Token) can be found in the portal under Authentification
@@ -54,69 +48,96 @@ const requestOptions = {
 return requestOptions
 }
 
-// Dont need this. Upgraded to particles bg
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id:'',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false
-  }
+    this.state = initialState;
+}
+
+loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+  }})
 }
 
 calculateFaceLocation = (data) => {
   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-  const image = document.getElementById('inputimage');
+  const image = document.getElementById('inputImage');
   const width = Number(image.width);
   const height = Number(image.height);
   console.log(width, height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.left_col * width),
+      rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
-  }
+  } 
 
   displayFaceBox = (box) => {
     this.setState({box: box});
   }
 
-  OnInputChange = (event) => {
+  onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    console.log(this.state.input)
-
+    fetch('https://vast-savannah-05715-00cda8775134.herokuapp.com/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        input: this.state.input
+      })
+    })
   const requestOptions = returnClarifai(this.state.input);
 
-    // app.models.predict('face-detection', this.state.input)
-    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifai(this.state.input))
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", requestOptions)
     .then(response => response.json())
-     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+     .then(response => {
+      if (response) {
+        fetch('https://vast-savannah-05715-00cda8775134.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+        })
+          .then(response => response.json())
+          .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+          .catch(console.log)
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
      .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -127,14 +148,17 @@ calculateFaceLocation = (data) => {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <ParticlesBg type="cobweb" bg={true} 
+        <ParticlesBg type="circle" bg={true} 
         // params={particlesOptions}
         />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
         { route === 'home' 
           ? <div>
               <Logo />
-              <Rank />
+              <Rank 
+                name={this.state.user.name} 
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
@@ -143,8 +167,8 @@ calculateFaceLocation = (data) => {
             </div>
           : (
             route === 'signin'
-            ? <Signin onRouteChange={this.onRouteChange}/>
-            : <Register onRouteChange={this.onRouteChange}/>
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
